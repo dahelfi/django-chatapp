72e3511d4b2e9e6df13f544daf3fd430f9bfd44c
Daniel Helfenstein <daniel.helfenstein@mail.de>
2022-10-04 18:51:42 +0200

    review_version

--- chat/views.py
+++ chat/views.py
@@ -12,37 +12,32 @@ from django.views.decorators.csrf import csrf_exempt
 
 @login_required(login_url='/login/')
 def index(request):
-    allUsers = User.objects.all
-    allChats = Chat.objects.filter(participant1 = request.user.id) | Chat.objects.filter(participant2 = request.user.id)
+    all_users = User.objects.all
+    all_chats = Chat.objects.filter(participant1 = request.user.id) | Chat.objects.filter(participant2 = request.user.id)
     if request.method == 'POST':
-        print("ich werde ausgeführt")
         user = User.objects.get(id = request.POST.get("userId"))
         new_user = Chat.objects.create(participant1 = request.user, participant2 = user)
         serialized_obj = serializers.serialize('json', [new_user, ])
         return JsonResponse(serialized_obj[1:-1], safe=False)
-   
-    return render(request, 'chat/index.html', {'users': allUsers, "chats": allChats})
+    return render(request, 'chat/index.html', {'users': all_users, "chats": all_chats})
+
 
 @login_required(login_url='/login/')
 def message_view(request, id):
-    print("ich werde ausgeführt mit der chat id: ", id)
-    allUsers = User.objects.all
-    allChats = Chat.objects.filter(participant1 = request.user.id) | Chat.objects.filter(participant2 = request.user.id)
+    all_users = User.objects.all
+    all_chats = Chat.objects.filter(participant1 = request.user.id) | Chat.objects.filter(participant2 = request.user.id)
     text_messages = Message.objects.filter(chat_id=id)
-    myChat = Chat.objects.get(id=id)
-    if(request.user.id == myChat.participant1.id):
-            recieverUser = myChat.participant2
+    my_chat = Chat.objects.get(id=id)
+    if(request.user.id == my_chat.participant1.id):
+            reciever_user = my_chat.participant2
     else:
-        recieverUser = myChat.participant1
+        reciever_user = my_chat.participant1
     if(request.method == "POST"):
-       
         text_messages = Message.objects.filter(chat_id=id)
-        new_message = Message.objects.create(text = request.POST["textmessage"], chat = myChat, author = request.user, reciever = recieverUser)
+        new_message = Message.objects.create(text = request.POST["textmessage"], chat = my_chat, author = request.user, reciever = reciever_user)
         serialized_obj = serializers.serialize('json', [new_message, ])
         return JsonResponse(serialized_obj[1:-1], safe=False)
-
-    return render(request, 'chat/chat.html', {'messages': text_messages,'users': allUsers, "chats": allChats, "recieverUser": recieverUser})
-
+    return render(request, 'chat/chat.html', {'messages': text_messages,'users': all_users, "chats": all_chats, "recieverUser": reciever_user, "chatId": id})
 
 
 @csrf_exempt
